c55d1d4caa62fe1f531a6ac14dca6567421359e5
Bernardo <126777966+BernardoSsilva@users.noreply.github.com>
2024-08-03 19:15:24 -0300

    Atualizar o Program.cs

--- src/Abigo.API/Program.cs
+++ src/Abigo.API/Program.cs
@@ -10,10 +10,12 @@ if (string.IsNullOrEmpty(connectionString))
 {
     throw new ArgumentException("Connection string 'DefaultConnection' is null or empty.");
 }
+
 // Add services to the container.
 builder.Services.AddEntityFrameworkNpgsql()
     .AddDbContext<AbigoDbAccess>(options => options.UseNpgsql(connectionString));
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -24,15 +26,15 @@ builder.Services.AddApplication();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-    app.UseSwagger(
-);
-    app.UseSwaggerUI(options=> 
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
-options.SwaggerEndpoint("/Swagger/v1/swagger.json", "production document");
-
-options.RoutePrefix = String.Empty;
-});
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "production document");
+        options.RoutePrefix = string.Empty; // This makes SwaggerUI the default route
+    });
+}
 
 app.UseHttpsRedirection();
 
