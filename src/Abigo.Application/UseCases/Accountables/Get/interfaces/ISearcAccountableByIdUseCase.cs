8c465a47f08f70b615a737295e7bf635ec8b4fd0
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-08-03 10:46:58 -0300

    :sparkles: Created Get Use cases

---
+++ src/Abigo.Application/UseCases/Accountables/Get/interfaces/ISearcAccountableByIdUseCase.cs
@@ -0,0 +1,14 @@
+﻿using Abigo.Communication.Responses.Accountable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abigo.Application.UseCases.Accountables.Get.interfaces
+{
+    public interface ISearcAccountableByIdUseCase
+    {
+        Task<AccountableDetailedResponse> Execute(string id);
+    }
+}
