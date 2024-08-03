1d3a1e1170f6e4814778b78ec8d0fd67ff6b4191
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-08-03 11:39:28 -0300

    :sparkles: Feat: Created delete locale use case

---
+++ src/Abigo.Application/UseCases/AvailableLocales/Delete/DeleteAvailableLocaleUseCase.cs
@@ -0,0 +1,50 @@
+﻿using Abigo.Application.UseCases.AvailableLocales.Delete.Interfaces;
+using Abigo.Domain.Models;
+using Abigo.Domain.Repositories;
+using Abigo.JWTAdmin;
+using AutoMapper;
+
+namespace Abigo.Application.UseCases.AvailableLocales.Delete
+{
+    public class DeleteAvailableLocaleUseCase : IDeleteAvailableLocaleUseCase
+    {
+        private readonly IAvailableLocalesRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public DeleteAvailableLocaleUseCase(IAvailableLocalesRepository repository, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task Execute(string localeId, string token)
+        {
+            var tokenAdmin = new AdminToken();
+
+            var tokenIsAvailable = tokenAdmin.ValidateToken(token);
+
+            if (!tokenIsAvailable)
+            {
+                throw new UnauthorizedAccessException("unhautorized");
+            }
+
+            var localeToDelete = await _repository.SearchDisponibleLocale(localeId);
+
+            if(localeToDelete is null)
+            {
+                throw new ArgumentException("not found")
+            }
+
+            var decodedToken = tokenAdmin.DecodeToken(token);
+            if(localeToDelete.AccountId != decodedToken.AccountId)
+            {
+                throw new UnauthorizedAccessException("unhautorized");
+            }
+
+            await _repository.DeleteLocale(localeId);
+            await _unitOfWork.Commit();
+        }
+    }
+}
