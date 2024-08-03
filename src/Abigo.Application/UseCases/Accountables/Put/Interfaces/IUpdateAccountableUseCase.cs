3e59a56581fc3f09e346dbcc1e845660a4558cb4
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-08-03 10:52:49 -0300

    :sparkles: Created update account use case

---
+++ src/Abigo.Application/UseCases/Accountables/Put/Interfaces/IUpdateAccountableUseCase.cs
@@ -0,0 +1,9 @@
+﻿using Abigo.Communication.Requests;
+
+namespace Abigo.Application.UseCases.Accountables.Put.Interfaces
+{
+    public interface IUpdateAccountableUseCase
+    {
+        Task Execute(AccountableRequestJson request, string id);
+    }
+}
---
+++ src/Abigo.Application/UseCases/Accountables/Put/UpdateAccoutableUseCase.cs
@@ -0,0 +1,38 @@
+﻿using Abigo.Application.UseCases.Accountables.Put.Interfaces;
+using Abigo.Communication.Requests;
+using Abigo.Domain.Entities;
+using Abigo.Domain.Models;
+using Abigo.Domain.Repositories;
+using AutoMapper;
+
+namespace Abigo.Application.UseCases.Accountables.Put
+{
+    public class UpdateAccoutableUseCase : IUpdateAccountableUseCase
+    {
+        private readonly IAccountablesRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public UpdateAccoutableUseCase(IAccountablesRepository repository, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task Execute(AccountableRequestJson request, string id)
+        {
+            var accountToUpdate = await _repository.SearchAccountable(id);
+
+            if(accountToUpdate is null)
+            {
+                throw new ArgumentException("not found");
+            }
+            var newAccoutableData = _mapper.Map<AccountableEntity>(request);
+            accountToUpdate = _mapper.Map(accountToUpdate, newAccoutableData);
+
+            await _repository.UpdateAccountable(accountToUpdate);
+            return;
+        }
+    }
+}
