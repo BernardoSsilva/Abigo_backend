7b958f3e8262193ae3a1ac8d594d110d024de699
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-08-03 01:25:35 -0300

    :sparkles: Feat: Created accountable request json

---
+++ src/Abigo.Communication/Requests/AccountableRequestJson.cs
@@ -0,0 +1,13 @@
+﻿namespace Abigo.Communication.Requests
+{
+    public class AccountableRequestJson
+    {
+        public string Name { get; set; } = string.Empty;
+        public string contactEmail { get; set; } = string.Empty;
+        public string contactPhone { get; set; } = string.Empty;
+        public string instagram { get; set; } = string.Empty;
+        public string ConnectionEmail { get; set; } = string.Empty;
+
+        public string AccessPassword { get; set; } = string.Empty;
+    }
+}
