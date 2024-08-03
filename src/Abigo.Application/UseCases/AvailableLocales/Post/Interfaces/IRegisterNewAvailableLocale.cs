184bd8196593eb662cf4cead05c964c378761e29
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-08-03 02:07:34 -0300

    :sparkles: Feat: Created register available locale use case

---
+++ src/Abigo.Application/UseCases/AvailableLocales/Post/Interfaces/IRegisterNewAvailableLocale.cs
@@ -0,0 +1,10 @@
+﻿using Abigo.Communication.Requests;
+using Abigo.Communication.Responses.AvailableLocales;
+
+namespace Abigo.Application.UseCases.AvailableLocales.Post.Interfaces
+{
+    public interface IRegisterNewAvailableLocale
+    {
+        Task<AvailableLocalesShortResponseJson> Execute(AvailableLocalesRequestJson request, string token);
+    }
+}
--- src/Abigo.JWTAdmin/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Abigo.JWTAdmin
-{
-    public class Class1
-    {
-
-    }
-}
