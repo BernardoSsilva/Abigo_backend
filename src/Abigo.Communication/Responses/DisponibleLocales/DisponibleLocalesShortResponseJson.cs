d63b7842f027c5b1c741ed5d57b6bac39ed4eb8f
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-08-03 01:35:12 -0300

    :sparkles: Feat: Created disponible locales short response

---
+++ src/Abigo.Communication/Responses/DisponibleLocales/DisponibleLocalesShortResponseJson.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abigo.Communication.Responses.DisponibleLocales
+{
+    public class DisponibleLocalesShortResponseJson
+    {
+        public string Id { get; set; } = string.Empty;
+        public string AccountId { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string NeighboorHood { get; set; } = string.Empty;
+        public string Street { get; set; } = string.Empty;
+        public int VacanciesNumber { get; set; } = 0;
+        public string contactPhone { get; set; } = string.Empty;
+
+        public string DonationKey { get; set; } = string.Empty;
+        public LocalesCategories Category { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
